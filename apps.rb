9f25325765dadf823e55bf501fc06af1b7be9799
delano <delano@solutious.com>
2009-06-06 12:24:16 -0400

    Renamed myip.rb to apps.rb

---
+++ apps.rb
@@ -0,0 +1,17 @@
+# set environment variables
+ENV['RACK_ENV'] ||= 'production'
+ENV['APP_ROOT'] ||= File.expand_path(File.join(File.dirname(__FILE__)))
+
+$:.unshift(ENV['APP_ROOT'])       # set load paths
+
+require 'rubygems'                # require
+require 'sinatra'                 # libs
+
+get '/ip' do
+  raw = @request.env["REMOTE_ADDR"]
+  raw.match(/^(\d+\.\d+\.\d+\.\d+),?/)
+  $1
+end
+
+get '/' do
+end
