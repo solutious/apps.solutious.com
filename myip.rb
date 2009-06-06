0f607924ce9a4bef2ae1857c8c2ed6ae8f439279
delano <delano@solutious.com>
2009-06-06 12:16:42 -0400

    New displays IP address

--- myip.rb
+++ myip.rb
@@ -1,7 +1,6 @@
 # set environment variables
 ENV['RACK_ENV'] ||= 'production'
 ENV['APP_ROOT'] ||= File.expand_path(File.join(File.dirname(__FILE__)))
-ENV['APP_ROOT_URI'] = '/'
 
 $:.unshift(ENV['APP_ROOT'])       # set load paths
 
@@ -9,7 +8,7 @@ require 'rubygems'                # require
 require 'sinatra'                 # libs
 
 get '/' do
-  'Hello world!'
+  raw = @request.env["REMOTE_ADDR"]
+  raw.match(/^(\d+\.\d+\.\d+\.\d+),?/)
+  $1
 end
-
-
